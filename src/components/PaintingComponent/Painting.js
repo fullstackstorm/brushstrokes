f45a8479bcccd24880b1556d4817f12a0a2866bd
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-29 12:35:40 -0700

    Style the viewing room.

--- src/components/PaintingComponent/Painting.js
+++ src/components/PaintingComponent/Painting.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Painting.css"
 
 function Painting({ painting }) {
   return (
-    <div>
-      <h2>
+    <div className="Card">
+      <h2 className="Card-title">
         {painting.name}, {painting.year}
       </h2>
-      <img src={painting.image} alt={painting.name} />
-      <h3>{painting.artist}</h3>
+      <img src={painting.image} alt={painting.name} className="Card-image" />
+      <h3 className="Card-artist">{painting.artist}</h3>
     </div>
   );
 }
--- src/components/ViewingRoomComponent/ViewingRoom.js
+++ src/components/ViewingRoomComponent/ViewingRoom.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Painting from "../PaintingComponent/Painting";
+import "./ViewingRoom.css"
 
 function ViewingRoom({ gallery }) {
   return (
-    <div>
+    <div className="Card-container">
       {gallery.map((painting) => (
         <Painting key={painting.id} painting={painting} />
       ))}
