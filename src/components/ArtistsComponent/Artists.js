12a527fe5c951470582f70d31569968e7438fd33
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-29 12:40:32 -0700

    Add container styling.

--- src/components/ArtistsComponent/Artists.js
+++ src/components/ArtistsComponent/Artists.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Artist from "../ArtistComponent/Artist";
+import "./Artists.css"
 
 function Artists({ artists }) {
   return (
-    <div>
+    <div className="Card-container">
       {artists.map((artist) => (
         <Artist key={artist.id} artist={artist} />
       ))}
