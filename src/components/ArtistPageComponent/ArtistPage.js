e3f28a0d18bc330a444f3b72386ea19926c9694c
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-29 03:23:07 -0700

    Build the form outline.

--- src/components/ArtistPageComponent/ArtistPage.js
+++ src/components/ArtistPageComponent/ArtistPage.js
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
+import Form from "../FormComponent/Form";
 
 function ArtistPage() {
   const location = useLocation();
@@ -7,15 +8,15 @@ function ArtistPage() {
 
   return (
     <div>
-      <p>
-        <img src={artist.image} alt={`self-portrait of ${artist.name}`} />
-        <h2>{artist.name}</h2>
-        <h3>
-          Born: {artist.born} Died: {artist.died}
-        </h3>
-        <p>{artist.biography}</p>
-      </p>
+      <img src={artist.image} alt={`self-portrait of ${artist.name}`} />
+      <h2>{artist.name}</h2>
+      <h3>
+        Born: {artist.born} Died: {artist.died}
+      </h3>
+      <p>{artist.biography}</p>
       <img src={artist.signature} alt="artist's signature" />
+      <Form artist={artist}></Form>
+      <br />
     </div>
   );
 }
--- src/components/FormComponent/Form.js
+++ src/components/FormComponent/Form.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {
+function Form({ artist }) {
+  const [name, setName] = useState("");
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    setName("");
+  }
+
+  return (
+    <div>
+      <h3>Interested in this artist?</h3>
+      <p>
+        Add their paintings to you personal art collection. Fill out the form
+        below to add to your collection.
+      </p>
+      <form onSubmit={handleSubmit}>
+        To add {artist.name}'s art to your collection, input your name:
+        <br />
+        <input type="text" value={name} onChange={handleNameChange} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
