d7212f58a7e00a458757d767868cccb7856ce26f
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-28 23:35:12 -0700

    Start with a home page.

--- src/components/AppComponent/App.js
+++ src/components/AppComponent/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Home from '../HomeComponent/Home';
+import ViewingRoom from '../ViewingRoomComponent/ViewingRoom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path={["/", "/viewing-room", "/artists"]}>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
---
+++ src/components/ViewingRoomComponent/ViewingRoom.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ViewingRoom() {
+
+}
+
+export default ViewingRoom;
