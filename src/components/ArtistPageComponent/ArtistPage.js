56698f2adcab2bc42686a295a1e1e3744e94f7f4
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-02 18:19:02 -0700

    Structure the artist's page.

--- src/components/ArtistPageComponent/ArtistPage.js
+++ src/components/ArtistPageComponent/ArtistPage.js
@@ -1,20 +1,28 @@
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
 import Form from "../FormComponent/Form";
+import Artist from "../ArtistComponent/Artist";
+import "./ArtistPage.css";
 
 function ArtistPage({ setCollection }) {
   const location = useLocation();
   const artist = location.state.artist;
 
   return (
-    <div>
-      <img src={artist.image} alt={`self-portrait of ${artist.name}`} />
-      <h2>{artist.name}</h2>
-      <h3>
-        Born: {artist.born} Died: {artist.died}
-      </h3>
-      <p>{artist.biography}</p>
-      <img src={artist.signature} alt="artist's signature" />
+    <div className="artist-page">
+      {" "}
+      <div>
+        <Artist className="floatingComponent" artist={artist} />
+        <h3 className="life">
+          Born: {artist.born} Died: {artist.died}
+        </h3>
+        <img
+          className="signature"
+          src={artist.signature}
+          alt="artist's signature"
+        />
+      </div>
+      <p className="biography">{artist.biography}</p>
       <Form artist={artist} setCollection={setCollection}></Form>
       <br />
     </div>
