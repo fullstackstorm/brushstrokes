60a7b58097b651f39ab8b674dc87b9f03a09b5ca
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-02 19:12:07 -0700

    Style the art collection.

--- src/components/PersonalCollectionComponent/PersonalCollection.js
+++ src/components/PersonalCollectionComponent/PersonalCollection.js
@@ -1,23 +1,29 @@
 import React from "react";
 import Painting from "../PaintingComponent/Painting";
 import { v4 as uuidv4 } from "uuid";
+import "./PersonalCollection.css";
+import "../PaintingComponent/Painting.css";
 
 function PersonalCollection({ user, collection }) {
   if (collection.length === 0) {
     return (
-      <div>
+      <div className="Collection-title">
         <h1>Your collection is currently empty!</h1>
       </div>
     );
   }
 
   return (
-    <div>
-      <h1>{user.name}'s Personal Art Collection</h1>
-      {collection.map((painting) => (
-        <Painting key={uuidv4()} painting={painting} />
-      ))}
-    </div>
+    <>
+      <h1 className="Collection-title">
+        {user.name}'s Personal Art Collection
+      </h1>
+      <div className="Collection-container">
+        {collection.map((painting) => (
+          <Painting key={uuidv4()} painting={painting} />
+        ))}
+      </div>
+    </>
   );
 }
 
